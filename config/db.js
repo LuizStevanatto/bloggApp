a188a2a6481693140613b8a9754898d637611a94
Luiz Stevanatto <luiz.stevanatto5@gmail.com>
2021-02-10 09:19:25 -0300

    Adding the .env on database

--- config/db.js
+++ config/db.js
@@ -1,2 +1,2 @@
-     
-module.exports = {mongoURI: "mongodb+srv://neto:tico2212123@cluster0.zdpxs.mongodb.net/blogapp?retryWrites=true&w=majority"};
+require('dotenv')  
+module.exports = {mongoURI: process.env.mongoURI};
